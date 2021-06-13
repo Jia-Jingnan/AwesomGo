62929f14186caaa1159a0651a9da420886f69b32
Jia-Jingnan <875480307@qq.com>
2021-06-13 13:31:35 +0800

    0613-go语言中变量定义的注意事项

--- Alpha/main.go
+++ Alpha/main.go
@@ -14,7 +14,7 @@ func main() {
 
 	// 变量定义方式1
 	var aplpha int
-	// 变量定义方式2
+	// 变量定义方式2，简短声明，只能在函数内部使用
 	bravo := "我是Bravo"
 	// 变量定义方式3
 	var charlie = "我是Charlie"
@@ -27,4 +27,8 @@ func main() {
 	var kilo = "我是Kilo"
 	juliet, kilo = kilo, juliet
 	fmt.Println("julet:"+juliet, "kilo:"+kilo)
+
+	// 定义变量注意事项:大写的变量可以被其他包引用
+	// 定义的变量为被引用会报编译错误
+	// var Lima = "我是Lima"
 }
