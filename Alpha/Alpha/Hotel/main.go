abb914ca70839a3d1ddc12f64ad44bbafbccc8cc
Jia-Jingnan <875480307@qq.com>
2021-06-14 01:02:08 +0800

    0614-指针定义及使用

--- Alpha/Alpha/Hotel/main.go
+++ Alpha/Alpha/Hotel/main.go
@@ -11,4 +11,20 @@ func main() {
 	var alpha int = 1
 	fmt.Printf("alpha的值=%d, 内存地址为%v\n", alpha, &alpha)
 
+	var bravo *int = &alpha
+	fmt.Printf("bravo的值为%v, 地址%v\n", bravo, &bravo)
+
+	fmt.Println(alpha)
+	*bravo = 200
+	fmt.Println(alpha)
+
+	var charlie int = 123
+	var delta *int = &charlie
+	fmt.Println(*delta, charlie)
+
+	/**
+	值类型：整型，浮点型，bool,array,string,栈中分配
+	引用类型:指针，slice，map，chan，interface，堆中分配
+	*/
+
 }
